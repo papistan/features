3b00fa16d13a2a5e3c073a627b839d052efebdf8
Jay <jpapisan@launchdarkly.com>
2020-09-16 00:39:37 -0700

    Changes opacity

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import FeaturesContainer from "../components/FeaturesContainer"
 
-import "./index.scss"
-
 function IndexPage(){
 
   return (
